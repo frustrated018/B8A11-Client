0e61b418c889432d6573298e2e2854b35a7b1e74
frustrated018 <niloyd018@gmail.com>
2023-11-05 15:50:34 +0600

    Initial Setup for the Client Side

---
+++ src/Router/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div className=" text-center text-5xl text-red-500 mt-40">Initial Setup</div>,
+    },
+  ]);
+
+export default router;
