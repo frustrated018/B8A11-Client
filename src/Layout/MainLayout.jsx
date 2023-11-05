99162867260ad17b4d96aafc95517c16ff7be652
frustrated018 <niloyd018@gmail.com>
2023-11-05 20:55:02 +0600

    Made a basic navbar

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../SharedComponents/Navbar/Navbar";
 
 const MainLayout = () => {
   return (
     <div>
-      <div className=" text-center text-5xl text-char mt-40">Main Layout</div>
+      <Navbar></Navbar>
       <Outlet></Outlet>
     </div>
   );
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,8 +2,6 @@ const Home = () => {
   return (
     <div>
       <h5 className=" text-center text-5xl text-accent mt-40">Home page</h5>
-      <h5 className=" text-center text-3xl text-accent ">Home page</h5>
-      <h5 className=" text-center text-5xl text-accent ">Home page</h5>
     </div>
   );
 };
