e27e72412b448c070ca1d7eb7abfa55a2d6e8671
frustrated018 <niloyd018@gmail.com>
2023-11-05 16:08:22 +0600

    Added my custom daisy ui themes

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <div className=" text-center text-5xl text-red-500 mt-40">Initial Setup</div>,
-    },
-  ]);
+  {
+    path: "/",
+    element: (
+      <div>
+        <div className=" text-center text-5xl text-primary mt-40">
+          Initial Setup
+        </div>
+        <div className=" text-center text-5xl text-char mt-40">
+          Different color
+        </div>
+        <div className=" text-center text-5xl text-neutral mt-40">
+          Different color 2
+        </div>
+      </div>
+    ),
+  },
+]);
 
 export default router;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,25 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        myLight: {
+          "primary": "#4254b8",
+          "secondary": "#bce9b4",
+          "accent": "#2441e5",
+          "neutral": "#121a3a",
+          "base-100": "#eff1fa",
+          "char":"#080B1C",
+        },
+        myDark: {
+          "primary": "#4759bd",
+          "secondary": "#1e4b16",
+          "accent": "#1a37db",
+          "neutral": "#121a3a",
+          "base-100": "#050710",
+        },
+      },
+    ],
+  },
 }
