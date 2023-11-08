af4e3653019f0f044ab38390ef3da1bd3238987d
frustrated018 <niloyd018@gmail.com>
2023-11-08 19:45:53 +0600

    Updated the room card

--- src/Pages/Rooms/RoomCard.jsx
+++ src/Pages/Rooms/RoomCard.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 const RoomCard = ({ room }) => {
-  const { _id, img, description, roomType, location } = room;
+  const { _id, img, description, pricePerNight, location } = room;
 
   return (
     <>
@@ -15,7 +15,7 @@ const RoomCard = ({ room }) => {
 
           <div className="relative p-4 sm:p-6 lg:p-8">
             <p className="text-sm font-medium uppercase tracking-widest text-accent">
-              {roomType}
+              ${pricePerNight}/Night
             </p>
 
             <p className="text-xl font-bold text-white sm:text-2xl">
