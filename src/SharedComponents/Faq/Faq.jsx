5e4a101c87fbd62ae5489728cc78d0fa1e86a703
frustrated018 <niloyd018@gmail.com>
2023-11-08 18:44:41 +0600

    Fixed the FAQ section

--- src/SharedComponents/Faq/Faq.jsx
+++ src/SharedComponents/Faq/Faq.jsx
@@ -1,14 +1,18 @@
 const Faq = () => {
   return (
     <>
-      <div className="space-y-4">
+      <div className="space-y-4 max-w-screen-xl mx-auto py-10 px-10 lg:px-0">
+        <h1 className="text-black font-body text-4xl text-center">
+          FAQ&apos;S
+        </h1>
+        {/* Questions 1 */}
         <details
           className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden"
           open
         >
           <summary className="flex cursor-pointer items-center justify-between gap-1.5">
             <h2 className="text-lg font-medium text-gray-900">
-              Lorem ipsum dolor sit amet consectetur adipisicing?
+              How should I behave in a ryokan to show respect for tradition?
             </h2>
 
             <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
@@ -28,17 +32,18 @@ const Faq = () => {
           </summary>
 
           <p className="mt-4 leading-relaxed text-gray-700">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
-            veritatis molestias culpa in, recusandae laboriosam neque aliquid
-            libero nesciunt voluptate dicta quo officiis explicabo consequuntur
-            distinctio corporis earum similique!
+            Guests are encouraged to be respectful and follow customary
+            etiquettes, such as removing shoes at the entrance, being punctual
+            for meals, and showing gratitude with a bow or polite phrases like
+            arigatou gozaimasu. Ryokan staff are happy to assist with any
+            questions or concerns during your stay.
           </p>
         </details>
-
+        {/* Question 2 */}
         <details className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
           <summary className="flex cursor-pointer items-center justify-between gap-1.5">
             <h2 className="text-lg font-medium text-gray-900">
-              Lorem ipsum dolor sit amet consectetur adipisicing?
+              Is it appropriate to wear a yukata in the ryokan
             </h2>
 
             <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
@@ -58,10 +63,62 @@ const Faq = () => {
           </summary>
 
           <p className="mt-4 leading-relaxed text-gray-700">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
-            veritatis molestias culpa in, recusandae laboriosam neque aliquid
-            libero nesciunt voluptate dicta quo officiis explicabo consequuntur
-            distinctio corporis earum similique!
+          Yes, many ryokans provide yukatas (casual kimono) for guests to wear during their stay. It is a customary and comfortable choice for moving around the inn.
+          </p>
+        </details>
+
+        {/* Question 3 */}
+        <details className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
+          <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+            <h2 className="text-lg font-medium text-gray-900">
+              Do I need to bring my own bedding?
+            </h2>
+
+            <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </span>
+          </summary>
+
+          <p className="mt-4 leading-relaxed text-gray-700">
+          No, ryokans provide traditional futon bedding as part of your stay. You will find your futon neatly prepared in your room for a comfortable nights sleep.
+          </p>
+        </details>
+        {/* Question 4 */}
+        <details className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
+          <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+            <h2 className="text-lg font-medium text-gray-900">
+            What is included in my ryokan stay?
+            </h2>
+
+            <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </span>
+          </summary>
+
+          <p className="mt-4 leading-relaxed text-gray-700">
+          Your stay typically includes a traditional Japanese meal (kaiseki), access to onsen (hot spring baths), and the opportunity to participate in cultural activities like tea ceremonies or ikebana (flower arranging).
           </p>
         </details>
       </div>
